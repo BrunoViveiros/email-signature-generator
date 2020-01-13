fede0936517b330b0efc6ebe138e3d6cdd344427
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-13 19:05:51 -0200

    Feito container de icones

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+const socialNetworks = [
+  "Behance",
+  "Blogger",
+  "Codepen",
+  "Email",
+  "Facebook",
+  "GitHub",
+  "Linkedin",
+  "Messenger",
+  "Reddit",
+  "Skype",
+  "StackOverflow",
+  "Twitter",
+  "Whatsapp",
+  "Youtube"
+];
+
+const buttons = document.querySelectorAll(".tab");
+const containers = document.querySelectorAll(".container");
+
+(init => {
+  buttons.forEach(button => {
+    button.addEventListener("click", handleTabs);
+  });
+})();
+
+function populateSocialNetworks(type) {
+  const container = document.querySelector(".icons-container > .icons");
+  container.innerHTML = "";
+
+  socialNetworks.forEach(item => {
+    let icon = document.createElement("img");
+    if (type == "round") {
+      icon.src = `./assets/icons/social-rounded/${item}.svg`;
+    } else if (type == "circle") {
+      icon.src = `./assets/icons/social-circle/${item}.svg`;
+    }
+    container.appendChild(icon);
+  });
+}
+
 function teste() {
   let valores = {};
   let meuForm = document.querySelectorAll("form input");
@@ -11,14 +52,13 @@ function teste() {
   console.log(valores);
 }
 
-const buttons = document.querySelectorAll(".tab");
-const containers = document.querySelectorAll(".container");
-
 function handleTabs(e) {
   const button = e.target;
   const buttonName = e.target.innerHTML;
   const buttonActive = e.target.classList.contains("-active");
-  const container = document.querySelector(`section[data-name='${buttonName}']`);
+  const container = document.querySelector(
+    `section[data-name='${buttonName}']`
+  );
 
   if (!buttonActive) {
     removeActive(buttons);
@@ -28,10 +68,6 @@ function handleTabs(e) {
   }
 }
 
-buttons.forEach(button => {
-  button.addEventListener("click", handleTabs);
-});
-
-function removeActive(element) {
+removeActive = element => {
   element.forEach(item => item.classList.remove("-active"));
-}
+};
