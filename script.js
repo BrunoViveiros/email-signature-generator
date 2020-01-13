b191733ef7c8b1349448c1dc89d1c9b51c971ac8
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-13 20:12:52 -0200

    Adicionado input de social ao click

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ const socialNetworks = [
   "Whatsapp",
   "Youtube"
 ];
-
 const buttons = document.querySelectorAll(".tab");
 const containers = document.querySelectorAll(".container");
 const iconTypes = document.querySelectorAll(".icon-type");
@@ -26,13 +25,13 @@ const iconTypes = document.querySelectorAll(".icon-type");
 
   iconTypes.forEach(type => {
     type.addEventListener("click", populateSocialNetworks);
-  })
+  });
 
   populateSocialNetworks();
 })();
 
 function populateSocialNetworks() {
-  const type = document.querySelector('input[name="icon-type"]:checked').value;
+  const type = checkIconType();
   const container = document.querySelector(".icons-container .icons");
   container.innerHTML = "";
 
@@ -43,6 +42,9 @@ function populateSocialNetworks() {
     } else if (type == "circle") {
       icon.src = `./assets/icons/social-circle/${item}.svg`;
     }
+    icon.dataset.name = item;
+    icon.addEventListener("click", addSocialNetwork);
+
     container.appendChild(icon);
   });
 }
@@ -76,6 +78,46 @@ function handleTabs(e) {
   }
 }
 
-removeActive = element => {
+function removeActive(element) {
   element.forEach(item => item.classList.remove("-active"));
-};
+}
+
+function createSocialNetwork(name) {
+  let form = document.querySelector(`section[data-name='social'] form`);
+  let container = document.createElement("div");
+  let input = document.createElement("input");
+  let label = document.createElement("label");
+  let image = document.createElement("img");
+
+  container.classList.add("inputs");
+
+  input.id = name;
+  input.name = name;
+  input.placeholder = name;
+  input.type = "text";
+
+  label.setAttribute("for", name);
+  label.innerText = name;
+
+  if (checkIconType() == "round") {
+    image.src = `./assets/icons/social-rounded/${name}.svg`;
+  } else if (checkIconType() == "circle") {
+    image.src = `./assets/icons/social-circle/${name}.svg`;
+  }
+  image.alt = `Icone do ${name}`;
+
+  container.appendChild(input);
+  container.appendChild(label);
+  container.appendChild(image);
+
+  form.appendChild(container);
+}
+
+function addSocialNetwork(e) {
+  name = e.target.dataset.name;
+  createSocialNetwork(name);
+}
+
+function checkIconType() {
+  return document.querySelector('input[name="icon-type"]:checked').value;
+}
